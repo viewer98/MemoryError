b20b184accd1f5dadc1482f9484831bc58fedf64
Viewer <viewerbr@live.com>
2025-01-14 01:08:11 -0300

    No waitUntil now

--- Lua_Scripts/viewers_vip_acadia_cutter.lua
+++ Lua_Scripts/viewers_vip_acadia_cutter.lua
@@ -3,6 +3,8 @@ local UTILS = require("viewer_utils")
 
 local acadia_tree_id = 109007
 local bank_chest_id = 107737
+local wood_box_id = -1
+local wood_box_capacity_per_item = 0
 
 local acadia_trees_locations = {
     [3180] = {
@@ -11,9 +13,20 @@ local acadia_trees_locations = {
     }
 }
 
-local animating = false
-
 API.SetDrawTrackedSkills(true)
+
+for key, wbox_id in pairs(UTILS.WOOD_BOX_IDS) do
+    if Inventory:Contains(wbox_id) then
+        wood_box_id = wbox_id
+        print("Found a wood box: ", key)
+        break
+    end
+end
+
+if wood_box_id > 0 then
+    wood_box_capacity_per_item = UTILS.get_wood_box_current_capacity_per_item(wood_box_id)
+    print("Wood box capacity per item: ", tostring(wood_box_capacity_per_item))
+end
 
 local function get_available_acadia_tree()
     local objs = API.ReadAllObjectsArray({ UTILS.OBJECT_TYPES.OBJECT }, { acadia_tree_id }, {})
@@ -29,85 +42,152 @@ local function get_available_acadia_tree()
     return nil
 end
 
-local function deposit_logs()
-    if not UTILS.is_local_player_animating() then
-        if not API.BankOpen2() then
-            print("Bank is not open, trying to open it")
-            API.DoAction_Object1(0x5, API.OFF_ACT_GeneralObject_route1, { bank_chest_id }, 50)
+local SCRIPT_STATE = {
+    IDLE = 1,
+    CHOPPING_LOGS = 2,
+    FILLING_WOOD_BOX = 3,
+    OPENING_BANK = 4,
+    EMPTYING_WOOD_BOX = 5,
+    DEPOSIT_LOGS = 6,
+    CLOSING_BANK = 7,
+}
 
-            UTILS.wait_for_condition(function()
-                print("Waiting for bank to be open")
-                return API.BankOpen2()
-            end, function()
-                return API.ReadPlayerMovin()
-            end, UTILS.random_float(1.8, 3)
-            )
-        else
-            print("Bank is open, trying to deposit inventory")
-            API.DoAction_Interface(0xffffffff, 0xffffffff, 1, 517, 39, -1, API.OFF_ACT_GeneralInterface_route)
-            UTILS.wait_for_condition(function()
-                print("Waiting for inventory to not be full")
-                return not API.InvFull_()
-            end, false, UTILS.random_float(1.8, 3)
-            )
+local function get_script_state_key(value)
+    for k, v in pairs(SCRIPT_STATE) do
+        if v == value then
+            return k
         end
     end
+end
+
+local function random_sleep()
+    UTILS.sleep_range(500, 5000)
+end
+
+local function get_current_state()
+    if API.PlayerLoggedIn() then
+        print("We are logged in")
+        if API.BankOpen2() then
+            print("Bank is open")
+            if wood_box_id > 0 then
+                print("We have a wood box")
+                if UTILS.count_wood_box_individual_items() > 0 then
+                    print("We need to empty the wood box into the bank")
+                    return SCRIPT_STATE.EMPTYING_WOOD_BOX
+                elseif Inventory:Contains(UTILS.LOG_IDS.ACADIA) then
+                    print("We need to deposit the logs from our inventory into the bank")
+                    return SCRIPT_STATE.DEPOSIT_LOGS
+                else
+                    print("We need to close the bank")
+                    return SCRIPT_STATE.CLOSING_BANK
+                end
+            else
+                print("We don't have a wood box")
+                if Inventory:Contains(UTILS.LOG_IDS.ACADIA) then
+                    print("We need to deposit our logs into the bank")
+                    return SCRIPT_STATE.DEPOSIT_LOGS
+                else
+                    print("We need to close the bank")
+                    return SCRIPT_STATE.CLOSING_BANK
+                end
+            end
+        else
+            print("Bank is not open")
+            if UTILS.is_local_player_idle() then
+                print("Our player is not animating and not moving")
+                if Inventory:IsFull() then
+                    print("Inventory is full")
+                    if wood_box_id > 0 then
+                        print("We have a wood box")
+                        if UTILS.get_wood_box_item_count(UTILS.LOG_IDS.ACADIA) >= wood_box_capacity_per_item then
+                            print("Wood box is full, we need to open the bank")
+                            return SCRIPT_STATE.OPENING_BANK
+                        else
+                            print("Wood box still has space, let's fill it")
+                            return SCRIPT_STATE.FILLING_WOOD_BOX
+                        end
+                    else
+                        print("Let's open the bank")
+                        return SCRIPT_STATE.OPENING_BANK
+                    end
+                else
+                    print("Let's chop logs")
+                    return SCRIPT_STATE.CHOPPING_LOGS
+                end
+            end
+        end
+    end
+
+    print("Nothing else returned, we're idling now")
+    return SCRIPT_STATE.IDLE
+end
+
+local function chop()
+    random_sleep()
+    local acadia_tree = get_available_acadia_tree()
+    if acadia_tree then
+        print("Found a tree, let's try to chop it")
+        API.DoAction_Object_Direct(0x3b, API.OFF_ACT_GeneralObject_route0, acadia_tree)
+        UTILS.sleep_range(500, 800)
+    else
+        print("Tree not found")
+    end
+end
+
+local function fill_wood_box()
+    random_sleep()
+    print("Filling wood box")
+    API.DoAction_Inventory1(wood_box_id, 0, 1, API.OFF_ACT_GeneralInterface_route)
+    UTILS.sleep_range(500, 600)
+end
+
+local function open_bank()
+    random_sleep()
+    print("Bank is not open, trying to open it")
+    API.DoAction_Object1(0x5, API.OFF_ACT_GeneralObject_route1, { bank_chest_id }, 50)
+    UTILS.sleep_range(500, 600)
+end
+
+local function empty_wood_box()
+    print("Emptying wood box")
+    API.DoAction_Bank_Inv(wood_box_id, 8, API.OFF_ACT_GeneralInterface_route2)
+    UTILS.sleep_range(500, 600)
+end
+
+local function deposit_logs()
+    print("Depositing logs")
+    API.DoAction_Bank_Inv(UTILS.LOG_IDS.ACADIA, 7, API.OFF_ACT_GeneralInterface_route2)
+    UTILS.sleep_range(500, 600)
 end
 
 local function close_bank()
     print("Bank is open, trying to close it")
     API.DoAction_Interface(0x24, 0xffffffff, 1, 517, 318, -1, API.OFF_ACT_GeneralInterface_route)
-    UTILS.wait_for_condition(function()
-        print("Waiting for bank to be closed")
-        return not API.BankOpen2()
-    end, false, UTILS.random_float(0.5, 1.5)
-    )
-end
-
-local function chop_tree()
-    if not UTILS.is_local_player_animating() then
-        print("Player is not animating")
-        local acadia_tree = get_available_acadia_tree()
-        if acadia_tree then
-            if not animating or acadia_tree.Distance >= 2 then
-                print("Found a tree, let's try to chop it")
-                API.DoAction_Object_Direct(0x3b, API.OFF_ACT_GeneralObject_route0, acadia_tree)
-                UTILS.wait_for_condition(function()
-                    print("Waiting for player to start animating")
-                    return UTILS.is_local_player_animating() or acadia_tree == nil
-                end, function()
-                    return API.ReadPlayerMovin()
-                end, UTILS.random_float(1.8, 3)
-                )
-            elseif animating and acadia_tree.Distance < 2 then
-                print("This tree was cut down but hasn't disappeared yet, let's wait.")
-            end
-            animating = false
-        else
-            print("Tree not found")
-        end
-    else
-        animating = true
-        print("Looks like we are animating")
-    end
+    UTILS.sleep_range(500, 600)
 end
 
 while (API.Read_LoopyLoop()) do
     UTILS:anti_idle()
-    if API.InvFull_() then
-        print("Inventory is full")
+
+    local current_state = get_current_state()
+    print(tostring(get_script_state_key(current_state)), " - ", tostring(current_state))
+
+    if current_state == SCRIPT_STATE.CHOPPING_LOGS then
+        chop()
+    elseif current_state == SCRIPT_STATE.FILLING_WOOD_BOX then
+        fill_wood_box()
+    elseif current_state == SCRIPT_STATE.OPENING_BANK then
+        open_bank()
+    elseif current_state == SCRIPT_STATE.EMPTYING_WOOD_BOX then
+        empty_wood_box()
+    elseif current_state == SCRIPT_STATE.DEPOSIT_LOGS then
         deposit_logs()
-    else
-        print("Inventory is not full")
-        if API.BankOpen2() then
-            close_bank()
-        else
-            print("Bank is not open")
-            chop_tree()
-        end
+    elseif current_state == SCRIPT_STATE.CLOSING_BANK then
+        close_bank()
     end
+
     print("RAM usage: ", UTILS.get_formatted_memory_usage())
-    UTILS.sleep_range(300, 1200)
+    UTILS.sleep_range(600, 1200)
 end
 
 package.loaded["api"] = nil
